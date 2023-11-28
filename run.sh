90248fe6a6161d479c7cd216927b248e46b9f0eb
rclovis <clovis.rabot@epitech.eu>
2023-11-28 16:37:20 +0100

    start network

--- run.sh
+++ run.sh
@@ -11,17 +11,21 @@ git clone https://github.com/kubernetes-sigs/kubespray.git
 
 # Install dependencies from ``requirements.txt``
 python3 -m venv kube_venv
+cd kubespray
 source kube_venv/bin/activate
-sudo pip3 install -r kubespray/requirements.txt
+sudo pip3 install -r requirements.txt
 
 # Generate inventory file
-cp -rfp kubespray/inventory/sample kubespray/inventory/mycluster
-cat kubespray/inventory/mycluster/group_vars/all/all.yml
-cat kubespray/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
+cp -rfp inventory/sample inventory/mycluster
+cat inventory/mycluster/group_vars/all/all.yml
+cat inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
 declare -a IPS=($1 $2)
-CONFIG_FILE=kubespray/inventory/mycluster/hosts.yaml python3 kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
+CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
+
+
+
 
 # Install kubernetes cluster
-ansible-playbook -i kubespray/inventory/mycluster/hosts.yaml --become --become-user=root kubespray/reset.yml
-ansible-playbook -i kubespray/inventory/mycluster/hosts.yaml --become --become-user=root kubespray/cluster.yml
+ansible-playbook -i inventory/mycluster/hosts.yaml --user=puta --become --become-user=root reset.yml
+ansible-playbook -i inventory/mycluster/hosts.yaml --user=puta --become --become-user=root cluster.yml
 

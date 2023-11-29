90a2330f32e1f549008f826da5520d8c71ca8a88
rclovis <clovis.rabot@epitech.eu>
2023-11-29 11:22:36 +0100

    kubespray setup

--- run.sh
+++ run.sh
@@ -5,27 +5,32 @@ if [ $# -lt 4 ]; then
     echo "Usage: $0 <ip node1> <ip node2> <ip jenkins> <ip docker>"
     exit 1
 fi
+# ask the user for the username
+read -p 'Username: ' USER
+# ask the user for the password
+read -sp 'Password: ' PASSWORD
 
 cd ansible/roles/kubernetes
 git clone https://github.com/kubernetes-sigs/kubespray.git
 
 # Install dependencies from ``requirements.txt``
 python3 -m venv kube_venv
-cd kubespray
 source kube_venv/bin/activate
+cd kubespray
 sudo pip3 install -r requirements.txt
 
 # Generate inventory file
 cp -rfp inventory/sample inventory/mycluster
+# edit inventory/mycluster/inventory.ini
+
 cat inventory/mycluster/group_vars/all/all.yml
 cat inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
 declare -a IPS=($1 $2)
-CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
+# CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
 
-
-
+ssh-copy-id -i ~/.ssh/id_rsa.pub $USER@$1
+ssh-copy-id -i ~/.ssh/id_rsa.pub $USER@$2
 
 # Install kubernetes cluster
-ansible-playbook -i inventory/mycluster/hosts.yaml --user=puta --become --become-user=root reset.yml
-ansible-playbook -i inventory/mycluster/hosts.yaml --user=puta --become --become-user=root cluster.yml
-
+ansible-playbook -i inventory/mycluster/hosts.yaml --user=$USER --become --become-user=root reset.yml -e ansible_become_pass=$PASSWORD -e kubectl_localhost=true -e kubeconfig_localhost=true
+ansible-playbook -i inventory/mycluster/hosts.yaml --user=$USER --become --become-user=root cluster.yml -e ansible_become_pass=$PASSWORD -e kubectl_localhost=true -e kubeconfig_localhost=true

fc0a8ea74a3c5a356cf436bcaaebf8c0494efb3a
rclovis <clovis.rabot@epitech.eu>
2023-11-29 23:22:06 +0100

    config file for kube working

--- run.sh
+++ run.sh
@@ -26,7 +26,7 @@ cp -rfp inventory/sample inventory/mycluster
 cat inventory/mycluster/group_vars/all/all.yml
 cat inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
 declare -a IPS=($1 $2)
-# CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
+CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
 
 ssh-copy-id -i ~/.ssh/id_rsa.pub $USER@$1
 ssh-copy-id -i ~/.ssh/id_rsa.pub $USER@$2
@@ -34,3 +34,5 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub $USER@$2
 # Install kubernetes cluster
 ansible-playbook -i inventory/mycluster/hosts.yaml --user=$USER --become --become-user=root reset.yml -e ansible_become_pass=$PASSWORD -e kubectl_localhost=true -e kubeconfig_localhost=true
 ansible-playbook -i inventory/mycluster/hosts.yaml --user=$USER --become --become-user=root cluster.yml -e ansible_become_pass=$PASSWORD -e kubectl_localhost=true -e kubeconfig_localhost=true
+
+cp inventory/mycluster/artifacts/admin.conf ../admin.conf

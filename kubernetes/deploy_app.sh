3f51101054998c424521c6c0ac09f17bc5e34846
rclovis <clovis.rabot@epitech.eu>
2023-11-30 21:49:27 +0100

    Helm fully functional

--- kubernetes/deploy_app.sh
+++ kubernetes/deploy_app.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# usage: ./deploy_app.sh <app name> <path to whanos.yaml> <repository image> <repository name> <repository tag>
-if [ $# -ne 5 ]; then
-    echo "You must provide 5 arguments: <app name> <path to whanos.yaml> <repository image> <repository name> <repository tag>"
+# usage: ./deploy_app.sh <app name> <path to whanos.yaml> <image>
+if [ $# -ne 3 ]; then
+    echo "You must provide 3 arguments: <app name> <path to whanos.yaml> <image>"
     exit 1
 fi
 
 SHABSOLUTE=$(cd "$(dirname "$0")"; pwd)
-echo $SHABSOLUTE
-
 EXECPATH=$(pwd)
-echo $EXECPATH
 
 rm -f $SHABSOLUTE/mychart/values.yaml
 touch $SHABSOLUTE/mychart/values.yaml
 cat $2 >> $SHABSOLUTE/mychart/values.yaml
 echo "" >> $SHABSOLUTE/mychart/values.yaml
-echo "image: $3/$4:$5" >> $SHABSOLUTE/mychart/values.yaml
+echo "image: $3" >> $SHABSOLUTE/mychart/values.yaml
 
 helm install $1 $SHABSOLUTE/mychart
